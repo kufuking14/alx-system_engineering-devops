930c7fde00e637f89d3a92740bc2a1976ef66d42
kufuking14 <kiflomnguse14@gmail.com>
2022-09-18 21:40:27 +0300

    My commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create a file using puppet
+# Creates a file
 file { '/tmp/school':
   ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
